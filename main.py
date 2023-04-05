f40e697441a745bcc2668ae43c3a14103b9bf8fd
shipowner99 <daban2@naver.com>
2023-04-05 22:27:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://shipowner99:nI7xmMwFjQN2@ep-snowy-morning-109972.ap-southeast-1.aws.neon.tech/neondb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
